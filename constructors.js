c0bc4aebfa5b47de8c92833a77b935f5dbcdd88b
GaganTut <gstut@live.com>
2017-03-22 19:05:03 -1000

    Passed Invoke function

--- constructors.js
+++ constructors.js
@@ -127,3 +127,56 @@ DamageSpell.prototype = Object.create(Spell.prototype, {
    * @param  {Spellcaster} target         The spell target to be inflicted.
    * @return {boolean}                    Whether the spell was successfully cast.
    */
+
+function Spellcaster(name, health, mana) {
+  this.name = name;
+  this.health = health;
+  this.mana = mana;
+  this.isAlive = true;
+}
+
+Spellcaster.prototype.inflictDamage = function(damage) {
+  if (this.health <= damage) {
+    this.health = 0;
+    this.isAlive = false;
+  } else {
+    this.health -= damage;
+  }
+};
+
+Spellcaster.prototype.spendMana = function(cost) {
+  if (this.mana >= cost) {
+    this.mana -= cost;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Spellcaster.prototype.invoke = function(spell, target) {
+  if (spell instanceof Spell === false) {
+    return false;
+  }
+  if (spell instanceof DamageSpell !== true) {
+    if (this.mana >= spell.cost) {
+      this.spendMana(spell.cost);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  if (target instanceof Spellcaster === false) {
+    return false;
+  }
+  if (spell instanceof DamageSpell) {
+    if (this.mana >= spell.cost) {
+      this.spendMana(spell.cost);
+      target.inflictDamage(spell.damage);
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+
